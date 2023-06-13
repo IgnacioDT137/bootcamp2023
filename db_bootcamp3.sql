73db5c40680285a1e62c6a447c815f9787ebb6f7
jhonavelasquez <101206060+jhonavelasquez@users.noreply.github.com>
2023-06-13 11:35:36 -0400

    Add files via upload
    Creación de Base de datos

---
+++ db_bootcamp3.sql
@@ -0,0 +1,177 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-06-2023 a las 17:33:52
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_bootcamp3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `encuesta`
+--
+
+CREATE TABLE `encuesta` (
+  `id` int(11) NOT NULL,
+  `id_evento` int(11) DEFAULT NULL,
+  `valoracion` int(11) DEFAULT NULL,
+  `comentarios` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `encuesta`
+--
+
+INSERT INTO `encuesta` (`id`, `id_evento`, `valoracion`, `comentarios`) VALUES
+(1, 1, 9, 'El evento estuvo genial. ¡Aprendí mucho!');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `evento`
+--
+
+CREATE TABLE `evento` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `semanal` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `evento`
+--
+
+INSERT INTO `evento` (`id`, `nombre`, `fecha`, `semanal`) VALUES
+(1, 'Conferencia de Desarrollo Web', '2023-06-15', FALSE);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registro_evento`
+--
+
+CREATE TABLE `registro_evento` (
+  `id` int(11) NOT NULL,
+  `id_usuario` varchar(20) DEFAULT NULL,
+  `id_evento` int(11) DEFAULT NULL,
+  `asistio` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `registro_evento`
+--
+
+INSERT INTO `registro_evento` (`id`, `id_usuario`, `id_evento`, `asistio`) VALUES
+(1, '123456789', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `rut` varchar(20) NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
+  `correo` varchar(100) DEFAULT NULL,
+  `psw` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`rut`, `nombre`, `correo`, `psw`) VALUES
+('123456789', 'Juan Pérez', 'juan@example.com', 'password123');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `encuesta`
+--
+ALTER TABLE `encuesta`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_evento` (`id_evento`);
+
+--
+-- Indices de la tabla `evento`
+--
+ALTER TABLE `evento`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `registro_evento`
+--
+ALTER TABLE `registro_evento`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_evento` (`id_evento`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`rut`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `encuesta`
+--
+ALTER TABLE `encuesta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `evento`
+--
+ALTER TABLE `evento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `registro_evento`
+--
+ALTER TABLE `registro_evento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `encuesta`
+--
+ALTER TABLE `encuesta`
+  ADD CONSTRAINT `encuesta_ibfk_1` FOREIGN KEY (`id_evento`) REFERENCES `evento` (`id`);
+
+--
+-- Filtros para la tabla `registro_evento`
+--
+ALTER TABLE `registro_evento`
+  ADD CONSTRAINT `registro_evento_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`rut`),
+  ADD CONSTRAINT `registro_evento_ibfk_2` FOREIGN KEY (`id_evento`) REFERENCES `evento` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
